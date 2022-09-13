7debfe144b816706cea12ae5622eb0d928e32b12
dmitriyuzki <dmitriyuzki@gmail.com>
2022-09-13 07:52:40 +0300

    Update shell scripts formatting

--- 1.docker/entrypoint.sh
+++ 1.docker/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ ! -f /etc/ssl/certs/nginx/selfsigned.crt ] || [ ! -f /etc/ssl/private/nginx/selfsigned.key ]; then
-    openssl req -x509                                         \
-                -nodes                                        \
-                -days 28                                      \
-                -newkey rsa:2048                              \
+    openssl req -x509 \
+                -nodes \
+                -days 28 \
+                -newkey rsa:2048  \
                 -keyout /etc/ssl/private/nginx/selfsigned.key \
-                -out /etc/ssl/certs/nginx/selfsigned.crt      \
+                -out /etc/ssl/certs/nginx/selfsigned.crt \
                 -subj "/C=US/ST=New York/L=New York City/O=Some Org/CN=*"                
     openssl dhparam -out /etc/nginx/dhparam.pem 1024
 fi
